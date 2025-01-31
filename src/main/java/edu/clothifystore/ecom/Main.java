416bf3bfabf1c41a74f08fa36b05f3e6807ce890
GodXero <shansathish33@gmail.com>
2025-01-31 14:47:07 +0530

    Update

--- src/main/java/edu/clothifystore/ecom/Main.java
+++ src/main/java/edu/clothifystore/ecom/Main.java
@@ -1,7 +1,7 @@
-package edu.fashionease.ecom;
+package edu.clothifystore.ecom;
 
 public class Main {
 	public static void main (String[] args) {
-		System.out.println(true);
+		Starter.main(args);
 	}
 }
---
+++ src/main/java/edu/clothifystore/ecom/Starter.java
@@ -0,0 +1,17 @@
+package edu.clothifystore.ecom;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+	public static void main (String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start (Stage stage) throws Exception {
+		stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource(""))));
+	}
+}
