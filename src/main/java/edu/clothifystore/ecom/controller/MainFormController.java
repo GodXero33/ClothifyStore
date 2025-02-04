bef4f86eb310b56c8f50f9cfcf1938ffe10a9f90
GodXero <shansathish33@gmail.com>
2025-02-04 21:30:58 +0530

    Update

--- src/main/java/edu/clothifystore/ecom/controller/MainFormController.java
+++ src/main/java/edu/clothifystore/ecom/controller/MainFormController.java
@@ -4,12 +4,5 @@ import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class MainFormController {
-	public AnchorPane navPanel;
 
-	private boolean isNavPanelOpen;
-
-	public void navPanelOpenButtonOnAction (ActionEvent actionEvent) {
-		this.navPanel.setPrefWidth(this.isNavPanelOpen ? 65 : 200);
-		this.isNavPanelOpen = !this.isNavPanelOpen;
-	}
 }
