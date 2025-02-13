09d6e525511ee1a4a5674910dae990883dab53ab
GodXero <shansathish33@gmail.com>
2025-02-13 10:23:09 +0530

    Some minor updates

--- src/main/java/edu/clothifystore/ecom/controller/form/menu/main/ReportsFormController.java
+++ src/main/java/edu/clothifystore/ecom/controller/form/menu/main/ReportsFormController.java
@@ -1,10 +1,17 @@
 package edu.clothifystore.ecom.controller.form.menu.main;
 
 import edu.clothifystore.ecom.controller.form.menu.MenuForm;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class ReportsFormController implements MenuForm {
 	@Override
 	public void update () {
+
+	}
+
+	@FXML
+	public void dailyEmployeeReportOnAction (ActionEvent actionEvent) {
 
 	}
 }
